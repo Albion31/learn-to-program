fe301842d9b4c5b05c9ef5a354120c253a5897c3
Lan Pham <lanpham312@gmail.com>
2017-10-28 18:15:21 +0100

    Adding Chapter 6 - ex2

---
+++ chapter06/ex2.rb
@@ -0,0 +1,8 @@
+# table of contents
+
+line_width = 50
+puts "Table of Contents".center(line_width)
+puts ""
+puts "Chapter 1: " + "Getting started".ljust(line_width/3) + "page  1".rjust(line_width/2)
+puts "Chapter 2: " + "Numbers".ljust(line_width/3) + "page  9".rjust(line_width/2)
+puts "Chapter 3: " + "Letters".ljust(line_width/3) + "page 13".rjust(line_width/2)
