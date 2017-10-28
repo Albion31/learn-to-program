c78b478ed978b08be6a08ad5f764c3e340ad7864
Lan Pham <lanpham312@gmail.com>
2017-10-28 18:33:28 +0100

    Adding Chapter 7 - ex1

---
+++ chapter07/ex1.rb
@@ -0,0 +1,15 @@
+# 99 bottles of beer on the wall
+
+number = 99
+
+while number > 2
+puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+puts "Take one down and pass it around, #{number -= 1} bottles of beer on the wall."
+end
+
+puts "2 bottles of beer on the wall, 2 bottles of beer."
+puts "Take one down and pass it around, 1 bottle of beer on the wall."
+puts "1 bottle of beer on the wall, 1 bottle of beer."
+puts "Take one down and pass it around, no more bottles of beer on the wall."
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
