ec9bc661eec0481dfb916d6747b63229038d567c
Lan Pham <lanpham312@gmail.com>
2017-10-28 17:25:37 +0100

    Adding Chapter 5 - ex2

---
+++ chapter05/ex1.rb
@@ -0,0 +1,9 @@
+#full name greeting
+puts "What is your first name? "
+first = gets.chomp
+puts "What is your middle name? "
+middle = gets.chomp
+puts "What is your last name? "
+last = gets.chomp
+
+puts "Hi #{first} #{middle} #{last}!"
