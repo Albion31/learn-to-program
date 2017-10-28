d1137a129f2a2571260b7a2274d6e4adae1ea8f3
Lan Pham <lanpham312@gmail.com>
2017-10-28 17:11:28 +0100

    Adding Chapter 2 - ex1

---
+++ chapter02/ex1.rb
@@ -0,0 +1,14 @@
+#hours in a year (non-leap year)
+puts 24 * 365
+
+#hours in a year (leap year)
+puts 24 * 366
+
+#minutes in a decade
+puts 60 * 24 * (365 * 10 + 2)
+
+#my age in seconds
+puts 60 * 60 * 24 * 365 * 33
+
+#author's age
+puts 1160000000 / 60 / 60 / 24 / 365
