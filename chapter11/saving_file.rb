6e732ed17474f882240110e110ff7e94616c619c
Lan Pham <lanpham312@gmail.com>
2017-11-03 16:15:26 +0000

    Saving and reading files

---
+++ chapter11/saving_file.rb
@@ -0,0 +1,16 @@
+filename = "ListerQuote.txt"
+test_string = "I promise that I swear absolutely that" +
+              "I will never mention gazpacho soup again"
+
+File.open (filename), "w" do |f|
+  f.write(test_string)
+end
+# File.open is to open the file and "w" is for write-access to the file
+# here we are adding the test_string to the file ListerQuote.txt
+
+read_string = File.read(filename)
+#this is to read the file
+
+puts (read_string == test_string)
+#this checks that the file saved in ListerQuote.txt is the same as what
+#is written is test_string
