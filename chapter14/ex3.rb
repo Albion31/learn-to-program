71b288427608864348f384a575aad62d7df7629f
Lan Pham <lanpham312@gmail.com>
2017-11-06 23:24:02 +0000

    Adding chapter14 - ex3

---
+++ chapter14/ex3.rb
@@ -0,0 +1,18 @@
+# Program logger
+
+def log(description, &block)
+  puts "We are starting #{description}..."
+  result = block.call
+  puts "...#{description} has ended and the result is #{result}"
+end
+
+log "outer block" do
+  log "some little block" do
+    31 * 12
+  end
+
+  log "yet another block" do
+    "Hello world " * 3
+  end
+  "Mission Completed"
+end
